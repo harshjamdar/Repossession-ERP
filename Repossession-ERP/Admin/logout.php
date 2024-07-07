e58fc253d8cfa02fa3e1c27d4827814bcc6305ec
AtharvaM41 <101097045+AtharvaM41@users.noreply.github.com>
2024-07-08 00:51:10 +0530

    Delete Repossession-ERP directory

--- Repossession-ERP/Admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();  // Unset all of the session variables
-session_destroy();  // Destroy the session
-header('Location: login.php');  // Redirect to login page
-exit;
-?>
